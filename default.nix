48396b0e0cc91f727e8b62458898295a30d7ac8a
cidkidnix <cidkidnix@protonmail.com>
2023-07-03 11:01:50 -0500

    Add nix pin and ghcid

--- default.nix
+++ default.nix
@@ -1,21 +1,22 @@
 {
-  reflex-platform ? import ./dep/reflex-platform { }
+  reflex-platform ? import ./dep/reflex-platform { },
+  nix ? import ./dep/nix
 }: (reflex-platform.project ({ pkgs, thunkSource, ... }: {
   name = "hnix-store";
   src = ./.;
   compiler-nix-name = "ghc8107";
   shells = p: with p; [ hnix-store-core hnix-store-remote ];
   shellTools = {
-    #haskell-language-server = "1.5.0.0";
     cabal-install = "3.2.0.0";
-    #tasty-discover = "5.0.0";
     haskell-language-server = "1.8.0.0";
   };
 })).extend (self: super: {
+  inherit nix;
   shells = super.shells // {
     ghc = self.shell-driver {
       exactDeps = false;
       additional = p: with p; [ tasty-discover ];
+      buildInputs = [ nix.packages.x86_64-linux.default super.pkgs.ghcid ];
     };
   };
 })
